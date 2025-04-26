04d1f323a070d9b86a519936ea37c07363c7ae8e
Aman Rohilla <rohillaa2004@gmail.com>
2025-04-26 23:35:20 +0530

    add: extend user routes to include change password, current user, update account, avatar, cover image, user channel profile, and watch history functionalities

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginUser, registerUser,logoutUser ,refreshAccesstoken} from "../controllers/user.controller.js";
+import { loginUser, registerUser,logoutUser ,refreshAccesstoken, changeCurrenPassword, getCurrentUser, updateDetails, updateUserAvatar, updateUserCoverimage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { getRounds } from "bcrypt";
 
 const router=Router();
 router.route("/register").post(
@@ -25,5 +26,14 @@ router.route("/login").post( loginUser)
 router.route("/logout").post(verifyJWT,logoutUser) 
 
 router.route("/refresh-token").post(refreshAccesstoken)
+router.route("/change-password").post(verifyJWT,changeCurrenPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverimage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
+
+
 
 export default router
